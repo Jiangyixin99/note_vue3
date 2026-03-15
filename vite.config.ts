abb14a6c019a34fe8463fcecb5170fc91231ad52
Jiangyixin99 <1903671157@qq.com>
2026-03-15 12:14:51 +0800

    更新 vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -9,7 +9,7 @@ import vue from '@vitejs/plugin-vue'
 // https://vite.dev/config/
 export default defineConfig({
   // base: process.env.NODE_ENV === 'production' ? '/note/' : '/',
-  base: '/note_vue3/',
+  base: '/',
 
   plugins: [
     vue({
