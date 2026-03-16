16687ac66048d50ba1ff2cc436a8679f67061f42
Jiangyixin99 <1903671157@qq.com>
2026-03-16 09:31:14 +0800

    init

--- src/env.d.ts
+++ src/env.d.ts
@@ -1,11 +1,2 @@
-declare module '*.vue' {
-    import { DefineComponent } from 'vue';
-    const component: DefineComponent<{}, {}, any>;
-    export default component;
-}
-// src/shims-md.d.ts
-declare module '*.md' {
-    import { ComponentOptions } from 'vue'
-    const component: ComponentOptions
-    export default component
-}
+/// <reference types="astro/client" />
+/// <reference path="../.astro/types.d.ts" />
